cbebf0ad4663ee6ad9417ef4f3062384f5ee6b22
Austin Story <lonnieastory@gmail.com>
2016-03-07 10:02:49 -0600

    Add google map integration/display of city and styling

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import SparkChart from '../components/chart';
+import GoogleMap from '../components/google-map';
 
 class WeatherList extends React.Component {
   renderWeather(cityData) {
@@ -8,13 +9,14 @@ class WeatherList extends React.Component {
       const temps = cityData.list.map(weather => weather.main.temp);
       const pressures = cityData.list.map(weather => weather.main.pressure);
       const humidities = cityData.list.map(weather => weather.main.humidity);
+      const {lon, lat}       = cityData.city.coord;
       
       return(  
         <tr key={ name }>
-          <td>{ name} </td>
-          <td><SparkChart data={temps     } color="yellow" /></td>
-          <td><SparkChart data={pressures } color="blue" /></td>
-          <td><SparkChart data={humidities} color="green" /></td>
+          <td><GoogleMap lat={lat} lon={lon} /></td>
+          <td><SparkChart data={temps     } color="yellow" units="K" /></td>
+          <td><SparkChart data={pressures } color="blue" units="hPa" /></td>
+          <td><SparkChart data={humidities} color="green" units="%" /></td>
         </tr>  
       );
   }    
@@ -25,9 +27,9 @@ class WeatherList extends React.Component {
             <thead>
                 <tr>
                     <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
+                    <th>Temperatur (K)</th>
+                    <th>Pressure (hPa)</th>
+                    <th>Humidity (%)</th>
                 </tr>
             </thead>
             <tbody>
